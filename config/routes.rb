eb95a34c411a98745359f244f097a889c501090d
remy lichani <rem.lichani@gmail.com>
2017-07-17 18:01:09 +0200

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :menus, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  namespace :account do
+    resources :menus, only: [:index, :show, :new, :create]
+    resources :bookings, only: [:index, :show]
+    resources :reservations, only: [:index, :show]
+    resource :profile, only: [:show]
+  end
 end
