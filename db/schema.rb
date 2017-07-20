632bf1ca6c5762f69104a9b26ae4e8de35e1a0d9
Constantin Pahl <constantin.pahl@gmail.com>
2017-07-20 15:35:23 +0200

    Add default value pending to Status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719132916) do
+ActiveRecord::Schema.define(version: 20170720153040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,12 @@ ActiveRecord::Schema.define(version: 20170719132916) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "date"
     t.integer  "price"
-    t.string   "status"
+    t.string   "status",      default: "pending"
     t.integer  "user_id"
     t.integer  "menu_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.text     "description"
     t.index ["menu_id"], name: "index_bookings_on_menu_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
