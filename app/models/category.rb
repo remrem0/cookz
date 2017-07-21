b4a5841875e583c8edc9a34fae5fc99d557c6247
Constantin Pahl <constantin.pahl@gmail.com>
2017-07-21 12:31:09 +0200

    fix seeds and validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :menus, dependent: :destroy
 
-  validates :name, presence: true, inclusion: { in: %w(French, Italian, Japanese, Chinese, Thaï, German, Spanish, Mexican)}
+  validates :name, presence: true, inclusion: { in: %w(French Italian Japanese Chinese Thaï German Spanish Mexican)}
 end
